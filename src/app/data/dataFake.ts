d6760b0dad00c9fd88961cdf6374accc6f8cc448
Camilabsm <camilabarbosasm@gmail.com>
2023-05-17 08:58:09 -0300

    final DIO project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "saiu novo filme",
+    "description": "descrição aqui",
+    "photoCover": "https://media.istockphoto.com/id/157528154/pt/foto/jornal.jpg?s=612x612&w=0&k=20&c=sxLZhAObemNwfNxhyW9dTINsA_aPiJLiSPOJjLsAqxA="
+  },
+  {
+    "id": "2",
+    "title": "saiu novo filme",
+    "description": "descrição aqui",
+    "photoCover": "aaa"
+  }
+]
